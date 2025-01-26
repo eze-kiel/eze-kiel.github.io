bad8491181136ffa53e8a7fb471fae7acc4d2179
Hugo Blanc <hugoblanc@fastmail.com>
2025-01-26 16:51:24 +0100

    Update media-data.js

--- media-data.js
+++ media-data.js
@@ -3,7 +3,7 @@ const mediaLibrary = [
         title: 'Pas de vagues',
         author: 'Teddy Lussi-Modeste',
         cover: 'https://www.themoviedb.org/t/p/w1280/xW5VofPowwcWcRcJtXGHm4xUh19.jpg',
-        rating: 2.5,
+        rating: 3,
         favorite: false,
         readDate: '2025-01-26',
         kind: 'movie',
